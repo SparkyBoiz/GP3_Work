df552039a32c6ea211030ff266d3be558e82be6b
Dylan MS <dylan.story@outlook.com>
2025-09-10 17:57:24 -0600

    WASD Movement

---
+++ Programming_Work/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private const float MoveSpeed = 5f;
+        private const float JumpForce = 5f;
+        private const float GroundCheckDistance = 1f;
+        public LayerMask groundLayer;
+
+        private Rigidbody _rb;
+        private Vector2 _moveInput;
+        private bool _jumpInput;
+        private bool _isGrounded;
+
+        private InputSystem_Actions _inputActions;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _inputActions = new InputSystem_Actions();
+
+            _inputActions.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+            _inputActions.Player.Move.canceled += _ => _moveInput = Vector2.zero;
+
+            _inputActions.Player.Jump.performed += _ =>
+            {
+                _jumpInput = true;
+            };
+        }
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+            Jump();
+        }
+
+        private void Move()
+        {
+            Vector3 direction = new Vector3(_moveInput.x, 0f, _moveInput.y);
+            Vector3 velocity = direction * MoveSpeed;
+            Vector3 currentVelocity = _rb.linearVelocity;
+            _rb.linearVelocity = new Vector3(velocity.x, currentVelocity.y, velocity.z);
+        }
+    
+        private void Jump()
+        {
+            _isGrounded = Physics.Raycast(transform.position, Vector3.down, GroundCheckDistance + 0.1f, groundLayer);
+
+            Debug.DrawRay(transform.position, Vector3.down * (GroundCheckDistance + 0.1f), _isGrounded ? Color.green : Color.red);
+
+            if (_jumpInput && _isGrounded)
+            {
+                _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            }
+        
+            _jumpInput = false;
+        }
+    }
+}
